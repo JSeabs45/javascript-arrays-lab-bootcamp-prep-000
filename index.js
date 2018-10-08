5227c3b9dc2b891112440865d47a516bb7faf08d
Joshua Seabolt <jseabolt45@hotmail.com>
2018-10-08 14:21:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Add your functions and code here
 
 function destructivelyAppendKitten() {
-  var kittens = ["Milo", "Otis", "Garfield"]
+  var kittens = ["Milo", "Otis", "Garfield"];
   kittens.push("Ralph");
   return kittens;
 }
